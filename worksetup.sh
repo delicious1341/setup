33cbb35ccb94432b3f4e7f14459cb500eb007bd3
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2023-04-15 12:35:52 +0000

    Create worksetup.sh

---
+++ worksetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# run this script from the directory containing setup files
+
+
+sudo apt update && sudo apt upgrade -y
+
+setup_dir=$(pwd)
+
+sudo apt install -y vim-gtk zathura texlive-full
+cp -r $setup_dir/.vimrc ~/
