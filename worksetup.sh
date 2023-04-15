6b31214764cd927833f3fc5d687a1ad75dc8c915
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2023-04-15 12:44:41 +0000

    Update worksetup.sh

--- worksetup.sh
+++ worksetup.sh
@@ -6,5 +6,5 @@ sudo apt update && sudo apt upgrade -y
 
 setup_dir=$(pwd)
 
-sudo apt install -y vim-gtk zathura texlive-full
+sudo apt install -y vim-gtk zathura texlive-full xournal
 cp -r $setup_dir/.vimrc ~/
