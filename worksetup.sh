2f1396d14597c48c3e4aca297da3c1cfb05cafc9
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2023-04-26 17:27:55 +0000

    Update worksetup.sh

--- worksetup.sh
+++ worksetup.sh
@@ -6,5 +6,13 @@ sudo apt update && sudo apt upgrade -y
 
 setup_dir=$(pwd)
 
-sudo apt install -y vim-gtk zathura texlive-full xournal
+sudo apt install -y vim-gtk zathura texlive-full xournal sxiv mpv
 cp -r $setup_dir/.vimrc ~/
+
+pip3 install --user yt-dlp
+
+xdg-mime default sxiv.desktop image/jpeg
+xdg-mime default sxiv.desktop image/png
+xdg-mime default sxiv.desktop image/webp
+xdg-mime default librewolf.desktop text/html
+xdg-mime default librewolf.desktop application/pdf
