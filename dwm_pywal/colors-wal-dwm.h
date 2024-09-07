a1e91a1cd526b13d12e8084228d7ebd76a1592ab
delicious1341 <130315844+delicious1341@users.noreply.github.com>
2024-09-07 21:41:55 +0000

    Create colors-wal-dwm.h

---
+++ dwm_pywal/colors-wal-dwm.h
@@ -0,0 +1,48 @@
+static char normfgcolor[]                = "{color15}";
+static char normbgcolor[]                = "{color0}";
+static char normbordercolor[]            = "{color8}";
+//static char normfloatcolor[]             = "#db8fd9";
+static char normfloatcolor[]             = "{color8}";
+
+static char selfgcolor[]                 = "{color15}";
+static char selbgcolor[]                 = "{color2}";
+static char selbordercolor[]             = "{color15}";
+//static char selfloatcolor[]              = "#005577";
+static char selfloatcolor[]              = "{color15}";
+
+static char titlenormfgcolor[]           = "{color15}";
+static char titlenormbgcolor[]           = "{color0}";
+static char titlenormbordercolor[]       = "{color8}";
+//static char titlenormfloatcolor[]        = "#db8fd9";
+static char titlenormfloatcolor[]        = "{color8}";
+
+static char titleselfgcolor[]            = "{color15}";
+static char titleselbgcolor[]            = "{color2}";
+static char titleselbordercolor[]        = "{color15}";
+//static char titleselfloatcolor[]         = "#005577";
+static char titleselfloatcolor[]         = "{color15}";
+
+static char tagsnormfgcolor[]            = "{color15}";
+static char tagsnormbgcolor[]            = "{color0}";
+static char tagsnormbordercolor[]        = "{color8}";
+//static char tagsnormfloatcolor[]         = "#db8fd9";
+static char tagsnormfloatcolor[]         = "{color8}";
+
+static char tagsselfgcolor[]             = "{color15}";
+static char tagsselbgcolor[]             = "{color2}";
+static char tagsselbordercolor[]         = "{color15}";
+//static char tagsselfloatcolor[]          = "#005577";
+static char tagsselfloatcolor[]          = "{color15}";
+
+//static char hidnormfgcolor[]             = "#005577";
+static char hidnormfgcolor[]             = "{color15}";
+//static char hidselfgcolor[]              = "#227799";
+static char hidselfgcolor[]              = "{color7}";
+static char hidnormbgcolor[]             = "{color0}";
+static char hidselbgcolor[]              = "{color0}";
+
+static char urgfgcolor[]                 = "{color15}";
+static char urgbgcolor[]                 = "{color1}";
+static char urgbordercolor[]             = "{color1}";
+//static char urgfloatcolor[]              = "#db8fd9";
+static char urgfloatcolor[]              = "{color8}";
